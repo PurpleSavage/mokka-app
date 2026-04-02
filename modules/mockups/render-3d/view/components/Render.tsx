277b01df56fabe11c693f46d90eb9952f0152b7e
PurpleSavage <jzuritapalomino@gmail.com>
2026-04-02 00:19:29 -0500

    agregar las posiciones iniciales de carga para el modelo 3d

--- modules/mockups/render-3d/view/components/Render.tsx
+++ modules/mockups/render-3d/view/components/Render.tsx
@@ -9,28 +9,19 @@ import ModelLoaderBar from './ModelLoaderBar'
 
 export default function Render() {
   const model = useSelector((state:RootState)=>state.render3D.modelLoadedInRender)
+  if (!model) return null
+  
   return (
-    <div className="h-full w-full ">
+    <div className="h-full w-full">
       <ModelLoaderBar/>
-      <Canvas shadows camera={{ position: [3, 3, 3], fov: 50 }}>
+      <Canvas shadows camera={{ position: model.cameraSettings.position, fov: model.cameraSettings.fov }}>
         <ambientLight intensity={1.5} />
         <pointLight position={[10, 10, 10]} intensity={2} castShadow />
         <directionalLight position={[-5, 5, 5]} intensity={1} />
         <Suspense fallback={null}>
-        <Environment preset="city" />
-          <Stage 
-            intensity={0.5} 
-            environment={null} 
-            adjustCamera={1.5}
-          >
-            {model?.modelUrl ? (
-              <ModelViewer url={model.modelUrl}  key={model.modelUrl}/>
-            ) : (
-              <mesh>
-                <boxGeometry />
-                <meshStandardMaterial color="royalblue" />
-              </mesh>
-            )}
+          <Environment preset="city" />
+          <Stage intensity={0.5} environment={null} adjustCamera={1.5}>
+            <ModelViewer url={model.modelUrl} key={model.modelUrl}/>
           </Stage>
           <ContactShadows opacity={0.4} scale={10} blur={2} far={4.5} />
         </Suspense>
